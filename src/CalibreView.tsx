64fd8049a9137868f8461f753bf4b06797286680
Cade <shibacomputer@users.noreply.github.com>
2024-09-15 13:20:14 +0200

    Updated to account for username and password.

--- src/CalibreView.tsx
+++ src/CalibreView.tsx
@@ -22,16 +22,22 @@ export class CalibreView extends ItemView {
     try {
       const iframe = container.createEl('iframe');
       iframe.setAttribute('sandbox', 'allow-forms allow-presentation allow-same-origin allow-scripts allow-modals allow-downloads allow-popups');
-      iframe.src = this.settings.replaceLocalIp
+      let address = this.settings.replaceLocalIp
         ? this.settings.address.replace('localhost', ip.address())
         : this.settings.address;
+      if (this.settings.username && this.settings.password) {
+        let url = new URL(address);
+        url.username = encodeURIComponent(this.settings.username);
+        url.password = encodeURIComponent(this.settings.password);
+        address = url.toString();
+      }
+      iframe.src = address;
     } catch (e) {
       console.error(e);
       const error = container.createDiv({ text: e.toString() });
       error.style.color = 'var(--text-title-h1)';
     }
   }
-
   async onClose() {
     // Nothing to clean up.
   }
